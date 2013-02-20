821bef6bcc53803a8f547f696ea5b53a81e9d57d
mattn <mattn.jp@gmail.com>
2013-02-20 11:21:15 +0900

    Add marshal

---
+++ marshal/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type Category struct {
+	Id int `json:"id"`
+	Name string `json:"name"`
+}
+
+type Book struct {
+	Id int `json:"id"`
+	Name string `json:"name"`
+	Categories []Category `json:"categories"`
+}
+
+func main() {
+	book := Book {
+		Id: 1,
+		Name: "プログラミング言語Goフレーズブック",
+		Categories: []Category {
+			{ Id: 3, Name: "プログラミング" },
+			{ Id: 4, Name: "技術" },
+		},
+	}
+
+	json.NewEncoder(os.Stdout).Encode(&book)
+}
